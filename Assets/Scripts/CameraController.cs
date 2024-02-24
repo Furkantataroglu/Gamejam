9aef3c8b90f6dfc86ef7bdb1b7a99f86c949f8ef
enesscevik <161017677+enesscevik@users.noreply.github.com>
2024-02-24 18:08:52 +0300

    Movement
    New Movement scripts is writed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,67 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class MoveAroundObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform target;
-    public Vector3 offset;
-    public bool useOffsetValues;
-    public float rotateSpeed;
-    public Transform pivot;
-    public float maxViewAngel;
-    public float minViewAngel;
-    void Start()
+    [SerializeField]
+    private float _mouseSensitivity = 3.0f;
+
+    private float _rotationY;
+    private float _rotationX;
+
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _distanceFromTarget = 3.0f;
+
+    private Vector3 _currentRotation;
+    private Vector3 _smoothVelocity = Vector3.zero;
+
+    [SerializeField]
+    private float _smoothTime = 0.2f;
+
+    [SerializeField]
+    private Vector2 _rotationXMinMax = new Vector2(-40, 40);
+
+    void Update()
     {
-        if(!useOffsetValues)
-        {
-            offset=target.position - transform.position;
-        }
-       pivot.transform.position = target.transform.position;
-       //pivot.transform.parent = target.transform;
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
 
-       pivot.transform.parent = null;
-        //mouse gizleme
-       Cursor.lockState = CursorLockMode.Locked;
-    }
+        _rotationY += mouseX;
+        _rotationX += mouseY;
 
-    // Update is called once per frame
-    void LateUpdate()
-    {  
-        pivot.transform.position = target.transform.position;
-        
-         //get the x position of the mouse and rotate the target
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        pivot.Rotate(0,horizontal,0);
-        //get the Y position of the mouse and rotate the pivot
-        float vertical = Input.GetAxis("Mouse Y")* rotateSpeed;
-        pivot.Rotate(-vertical,0,0);
+        // Apply clamping for x rotation 
+        _rotationX = Mathf.Clamp(_rotationX, _rotationXMinMax.x, _rotationXMinMax.y);
 
-        //camera rotation limit/up down
-        if(pivot.rotation.eulerAngles.x >maxViewAngel && pivot.rotation.eulerAngles.x <180f)
-        {
-            pivot.rotation = Quaternion.Euler(45f,0,0);
-        }
-        if(pivot.rotation.eulerAngles.x >180f && pivot.rotation.eulerAngles.x < 360f + minViewAngel)
-        {
-            pivot.rotation = Quaternion.Euler(360f + minViewAngel,0,0);
-        }
-        //move the camera based on the current rotation of the target and original offset
-        float desiredYAngle = pivot.eulerAngles.y;
-        float desiredXAngle = pivot.eulerAngles.x;
-        
-        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle,0);
-        transform.position=target.position-(rotation*offset);
+        Vector3 nextRotation = new Vector3(_rotationX, _rotationY);
 
+        // Apply damping between rotation changes
+        _currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, _smoothTime);
+        transform.localEulerAngles = _currentRotation;
 
-        if(transform.position.y < target.position.y)
-        {
-            transform.position = new Vector3(transform.position.x , target.position.y -0.5f,transform.position.z);
-        }
-        
-        //transform.position = target.position  - offset;
-        transform.LookAt(target.transform);
-
-        
+        // Substract forward vector of the GameObject to point its forward vector to the target
+        transform.position = _target.position - transform.forward * _distanceFromTarget;
     }
 }
