ccc13d4dc6aae25617151bd222a109eecf1ebb25
furkanttrgl <furkantatar2424@gmail.com>
2024-02-24 13:30:08 +0300

    version0.4

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,6 +10,8 @@ public class CameraController : MonoBehaviour
     public bool useOffsetValues;
     public float rotateSpeed;
     public Transform pivot;
+    public float maxViewAngel;
+    public float minViewAngel;
     void Start()
     {
         if(!useOffsetValues)
@@ -17,21 +19,36 @@ public class CameraController : MonoBehaviour
             offset=target.position - transform.position;
         }
        pivot.transform.position = target.transform.position;
-       pivot.transform.parent = target.transform;
+       //pivot.transform.parent = target.transform;
+
+       pivot.transform.parent = null;
         //mouse gizleme
        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void LateUpdate()
-    {   //get the x position of the mouse and rotate the target
+    {  
+        pivot.transform.position = target.transform.position;
+        
+         //get the x position of the mouse and rotate the target
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.Rotate(0,horizontal,0);
+        pivot.Rotate(0,horizontal,0);
         //get the Y position of the mouse and rotate the pivot
         float vertical = Input.GetAxis("Mouse Y")* rotateSpeed;
-        pivot.Rotate(-vertical,0,0);
+        //pivot.Rotate(-vertical,0,0);
+
+        //camera rotation limit/up down
+        if(pivot.rotation.eulerAngles.x >maxViewAngel && pivot.rotation.eulerAngles.x <180f)
+        {
+            pivot.rotation = Quaternion.Euler(45f,0,0);
+        }
+        if(pivot.rotation.eulerAngles.x >180f && pivot.rotation.eulerAngles.x < 360f + minViewAngel)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngel,0,0);
+        }
         //move the camera based on the current rotation of the target and original offset
-        float desiredYAngle = target.eulerAngles.y;
+        float desiredYAngle = pivot.eulerAngles.y;
         float desiredXAngle = pivot.eulerAngles.x;
         
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle,0);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class Movement : MonoBehaviour
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityScale;
+    public Transform pivot;
+    public float rotateSpeed; 
 
     
     // Start is called before the first frame update
@@ -50,6 +52,13 @@ public class Movement : MonoBehaviour
         }
         moveDirection.y = moveDirection.y + (Physics.gravity.y  * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
+
+        if(Input.GetAxis("Horizontal") !=0 || Input.GetAxis("Vertical") !=0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+        }
+
+
     }
    
     
