e5c00bc230d314c1fa16cf608e45279b20247e52
furkanttrgl <furkantatar2424@gmail.com>
2024-02-25 11:01:15 +0300

    HATALIRESPAWN
    hatalı respawn hali fixleyeceğim

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth;
+    public float flashingLength = 0.1f;
+    private float flashingCounter;
+    public Renderer playerRenderer;
+    private bool isRespawning;
+    public Transform RespawnPoint1;
+
+    private int currentHealth;
+    
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (flashingCounter > 0)
+        {
+            flashingCounter -= Time.deltaTime;
+            if (flashingCounter <= 0)
+            {
+                playerRenderer.enabled = !playerRenderer.enabled;
+                flashingCounter = flashingLength;
+            }
+        }
+
+        if (currentHealth <= 0 && !isRespawning)
+        {
+            isRespawning = true;
+            flashingCounter = flashingLength;
+            playerRenderer.enabled = false;
+            Respawn();
+        }
+    }
+
+    public void HurtPlayer(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isRespawning)
+        {
+            isRespawning = true;
+            flashingCounter = flashingLength;
+            playerRenderer.enabled = false;
+            Respawn();
+        }
+    }
+
+    public void HealPlayer(int healAmount)
+    {
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public void Respawn()
+    { 
+        PlayerInput playerInput = GetComponent<PlayerInput>();
+        playerInput.transform.position = RespawnPoint1.position;
+        currentHealth = maxHealth;
+        isRespawning = false;
+        Debug.Log("Respawned");
     }
 }
